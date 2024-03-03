0272dd62a8361be049f67eea124bbb6eda22cef8
arsiro01 <avsirotiya.01@gmail.com>
2024-03-03 13:58:31 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'speed_breaker_pothole',
+    version= '0.0.0',
+    author= 'Aviral Sirotiya',
+    author_email= 'avsirotiya.01',
+    packages= find_packages(),
+    install_requires = []
+
+)
